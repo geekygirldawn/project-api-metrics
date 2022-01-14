049633a4c9d55288610401178d86bdc7061cf61e
Dawn M. Foster <fosterd@vmware.com>
2022-01-14 15:47:25 +0000

    improved some comments and error messages

--- scripts/repo_activity.py
+++ scripts/repo_activity.py
@@ -34,6 +34,8 @@ from os.path import dirname, join
 from common_functions import read_key
 
 def make_query(after_cursor = None):
+    """Creates and returns a GraphQL query with cursor for pagination"""
+
     return """query RepoQuery($org_name: String!) {
              organization(login: $org_name) {
                repositories (first: 100 after: AFTER){
@@ -86,15 +88,27 @@ def make_query(after_cursor = None):
         "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
     )
 
-# Read GitHub key from file
+# Read GitHub key from file using the read_key function in 
+# common_functions.py
 try:
     api_token = read_key('gh_key')
 
 except:
-    print("Error reading GH Key. Exiting")
+    print("Error reading GH Key. This script depends on the existance of a file called gh_key containing your GitHub API token. Exiting")
     sys.exit()
 
 def get_repo_data(api_token):
+    """Executes the GraphQL query to get repository data from one or more GitHub orgs.
+
+    Parameters
+    ----------
+    api_token : str
+        The GH API token retrieved from the gh_key file.
+
+    Returns
+    -------
+    repo_info_df : pandas.core.frame.DataFrame
+    """
     import requests
     import json
     import pandas as pd
@@ -197,4 +211,4 @@ try:
     repo_info_df.to_csv(file_path, index=False)
 
 except:
-    print('Could not write to csv file. Exiting')
+    print('Could not write to csv file. This may be because the output directory is missing or you do not have permissions to write to it. Exiting')
